0e5a380322ea606b709024603355ecc9f4070104
Kristijan Sedlak <xpepermint@gmail.com>
2020-05-20 01:17:46 +0200

    Fix chunked writer

--- src/write.rs
+++ src/write.rs
@@ -38,37 +38,48 @@ pub async fn write_exact<I, O>(input: &mut I, output: &mut O, length: usize) -> 
     relay_exact(input, output, length).await
 }
 
-pub async fn write_chunks<I, O>(input: &mut I, output: &mut O, lengths: (usize, usize)) -> Result<usize, Error>
+pub async fn write_chunks<I, O>(input: &mut I, output: &mut O, limits: (Option<usize>, Option<usize>)) -> Result<usize, Error>
     where
     I: Read + Unpin,
     O: Write + Unpin,
 {
-    let (chunklen, datalen) = lengths;
-    let mut total = 0;
+    let (chunklimit, datalimit) = limits;
+    let chunksize = match chunklimit {
+        Some(chunksize) => chunksize,
+        None => 1024,
+    };
+    let mut total = 0; // all written bytes
+    let mut length = 0; // data written bytes
     
     loop {
-        let chunksize = match datalen - total < chunklen {
-            true => datalen - total, // do not read more than necessary
-            false => chunklen,
-        };
+        println!("===========> total:{} + chunksize:{} > datalimit:{:?}", total, chunksize, datalimit);
 
-        if chunksize == 0 {
-            write_all(output, b"0\r\n").await?;
-            write_all(output, b"\r\n").await?;
-            break;
-        }
+        let chunksize = match datalimit {
+            Some(datalimit) => match length + chunksize > datalimit {
+                true => datalimit - length,
+                false => chunksize,
+            },
+            None => chunksize,
+        };
 
         let mut bytes = vec![0u8; chunksize];
         let size = match input.read(&mut bytes).await {
             Ok(size) => size,
             Err(_) => return Err(Error::UnableToRead),
         };
-        total += size;
+        bytes = bytes[0..size].to_vec();
+        length += size;
 
-        write_all(output, format!("{:x}\r\n", chunksize).as_bytes()).await?;
-        write_all(output, &bytes).await?;
-        write_all(output, b"\r\n").await?;
+        println!("===========> size:{}", size);
+
+        total += write_all(output, format!("{:x}\r\n", size).as_bytes()).await?;
+        total += write_all(output, &bytes).await?;
+        total += write_all(output, b"\r\n").await?;
         flush_write(output).await?;
+
+        if size == 0 {
+            break;
+        }
     }
 
     Ok(total)
@@ -89,8 +100,12 @@ mod tests {
     #[async_std::test]
     async fn writes_chunks() {
         let mut output = Vec::new();
-        let size = write_chunks(&mut "0123456789".as_bytes(), &mut output, (3, 10)).await.unwrap();
-        assert_eq!(size, 10);
+        let size = write_chunks(&mut "0123456789".as_bytes(), &mut output, (Some(3), None)).await.unwrap();
+        assert_eq!(size, 35);
         assert_eq!(output, "3\r\n012\r\n3\r\n345\r\n3\r\n678\r\n1\r\n9\r\n0\r\n\r\n".as_bytes());
+        let mut output = Vec::new();
+        let size = write_chunks(&mut "0123456789".as_bytes(), &mut output, (Some(3), Some(4))).await.unwrap();
+        assert_eq!(size, 19);
+        assert_eq!(output, "3\r\n012\r\n1\r\n3\r\n0\r\n\r\n".as_bytes());
     }
 }
