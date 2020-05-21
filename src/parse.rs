b3504f891162ce4d3810d56fc4d8fa0d417fc160
Kristijan Sedlak <xpepermint@gmail.com>
2020-05-21 16:04:03 +0200

    Add uri parse

--- src/parse.rs
+++ src/parse.rs
@@ -1,10 +1,17 @@
-use std::io::Error;
+use std::io::{Error, ErrorKind};
 use std::convert::TryFrom;
 use crate::{Method, Status, Version};
 
 pub fn parse_method(data: Vec<u8>) -> Result<Method, Error> {
     let data: &[u8] = &data;
     Method::try_from(data)
+}
+
+pub fn parse_uri(data: Vec<u8>) -> Result<String, Error> {
+    match String::from_utf8(data) {
+        Ok(uri) => Ok(uri),
+        Err(e) => Err(Error::new(ErrorKind::InvalidData, e.to_string()))
+    }
 }
 
 pub fn parse_status(data: Vec<u8>) -> Result<Status, Error> {
